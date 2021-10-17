ecafd39ed27f27b59a0d55be59ed7f50f2148e1c
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-17 22:26:43 +0400

    premiers tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ObjectifComponent } from './objectif/objectif.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'objectif', component: ObjectifComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ObjectifComponent } from './objectif/objectif.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ObjectifComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
