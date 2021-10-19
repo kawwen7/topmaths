456116fa782a0eea3230c1bdcb9fbb0d9a3591e9
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-19 18:28:18 +0400

    feat: la liste des objectifs affiche maintenant les niveaux, thèmes et sous-thèmes

--- src/app/objectifs/objectifs.component.ts
+++ src/app/objectifs/objectifs.component.ts
@@ -1,9 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-interface Lien {
-  reference: string;
-  titre: string
+/**
+ * Type d'objet de toutes les lignes qui seront affichées
+ * Si c'est un niveau (5e, 4e...), l'attribut niveau sera complété et les autres seront null
+ * Si c'est un thème (Nombres et Calculs, Géométrie...), l'attribut theme sera complété et les autres seront null etc.
+ * De cette façon, les *ngIf du html sauront comment les afficher
+ */
+interface Ligne {
+  niveau?: string;
+  theme?: string;
+  sous_theme?: string;
+  reference?: string;
+  titre?: string
 }
 @Component({
   selector: 'app-objectifs',
@@ -11,21 +20,24 @@ interface Lien {
   styleUrls: ['./objectifs.component.css']
 })
 export class ObjectifsComponent implements OnInit {
-  liens: Lien[]
+  lignes: Ligne[]
 
   constructor(public http: HttpClient) {
-    this.liens = []
+    this.lignes = []
   }
 
   ngOnInit(): void {
     this.http.get('assets/data/objectifs.json').subscribe(
       (data: any) => {
-        this.liens = []
+        this.lignes = [] // va contenir toutes les lignes à afficher.
         for (const niveau of data) {
+          this.lignes.push({ niveau: niveau.niveau })
           for (const theme of niveau.themes) {
-            for (const sousTheme of theme.sous_themes) {
-              for (const reference of sousTheme.objectifs) {
-                this.liens.push(reference)
+            this.lignes.push({ theme: theme.nom })
+            for (const sous_theme of theme.sous_themes) {
+              this.lignes.push({ sous_theme: sous_theme.nom })
+              for (const reference of sous_theme.objectifs) {
+                this.lignes.push(reference)
               }
             }
           }
