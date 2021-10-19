ccb9c2400ba55942acf209ba3ff9c70d6ae062f3
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-19 17:39:26 +0400

    chore: Meilleur formattage du .json et un bien meilleur parcours de ce dernier

--- src/app/objectif/objectif.component.ts
+++ src/app/objectif/objectif.component.ts
@@ -14,18 +14,18 @@ interface Exercice {
   styleUrls: ['./objectif.component.css']
 })
 export class ObjectifComponent implements OnInit {
-  ref : string
-  titre : string
-  rappelDuCoursHTML : string
-  rappelDuCoursImage : string
-  slugVideo : string
-  VideoSrc : string
-  auteurVideo : string
-  lienAuteurVideo : string
-  exercices : Exercice[]
+  reference: string
+  titre: string
+  rappelDuCoursHTML: string
+  rappelDuCoursImage: string
+  slugVideo: string
+  VideoSrc: string
+  auteurVideo: string
+  lienAuteurVideo: string
+  exercices: Exercice[]
 
-  constructor(public http : HttpClient, private route: ActivatedRoute) {
-    this.ref = ''
+  constructor(public http: HttpClient, private route: ActivatedRoute) {
+    this.reference = ''
     this.titre = ''
     this.rappelDuCoursHTML = ''
     this.rappelDuCoursImage = ''
@@ -38,53 +38,44 @@ export class ObjectifComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
-      this.ref = this.route.snapshot.params['ref'];
+      this.reference = this.route.snapshot.params['ref'];
     });
-    this.http.get('assets/data/objectifs.json').subscribe(
-      (data) => {
-        let json = JSON.parse(JSON.stringify(data))
-        let trouve = false
-        // On cherche dans le json la bonne référence
-        for (let niveau in json) {
-          for (let theme in json[niveau]) {
-            for (let sousTheme in json[niveau][theme]) {
-              for (let reference in json[niveau][theme][sousTheme]) {
-                if (json[niveau][theme][sousTheme][reference] == json[niveau][theme][sousTheme][this.ref]) {
-                  // Une fois qu'on l'a trouvée, on modifie les attributs
-                  const objectif = json[niveau][theme][sousTheme][reference]
-                  this.titre = objectif.titre
-                  this.rappelDuCoursHTML = objectif.rappelDuCoursHTML
-                  if (objectif.rappelDuCoursImage == '') {
-                    this.rappelDuCoursImage = '' // Au cas où l'attribut ne serait pas réinitialisé lors d'un changement de référence
-                  } else {
-                    this.rappelDuCoursImage = '../assets/img/' + objectif.rappelDuCoursImage
-                  }
-                  this.slugVideo = objectif.slugVideo
-                  this.VideoSrc = "https://www.youtube.com/embed/" + this.slugVideo
-                  this.auteurVideo = objectif.auteurVideo
-                  this.lienAuteurVideo = objectif.lienAuteurVideo
-                  this.exercices = [] // Au cas où l'attribut ne serait pas réinitialisé lors d'un changement de référence
-                  // Le nombre d'exercices varie selon la référence, on a donc quelque chose de dynamique
-                  for (let i = 0; i < objectif.exercices.length; i++) {
-                    if (objectif.exercices[i].slug != '') {
-                      this.exercices.push({
-                        couleur: objectif.exercices[i].couleur,
-                        slug: objectif.exercices[i].slug,
-                        lien: 'https://coopmaths.fr/exercice.html?ex=' + objectif.exercices[i].slug + 'i=0&v=e'})
-                      }
-                  }
-                  trouve = true // Je n'ai pas trouvé plus élégant pour sortir des boucles lorsque la référence a été trouvée x)
+    this.http.get('assets/data/objectifs.json').subscribe((data: any) => {
+      // On cherche dans le json la bonne référence
+      data.find((niveau: any) => {
+        return niveau.themes.find((theme: any) => {
+          return theme.sous_themes.find((sous_theme: any) => {
+            return sous_theme.objectifs.find((objectif: any) => {
+              // Une fois qu'on l'a trouvée, on modifie les attributs
+              if (objectif.reference == this.reference) {
+                this.titre = `${objectif.reference} : ${objectif.titre}`
+                this.rappelDuCoursHTML = objectif.rappelDuCoursHTML
+                if (objectif.rappelDuCoursImage == '') {
+                  this.rappelDuCoursImage = '' // Au cas où l'attribut ne serait pas réinitialisé lors d'un changement de référence
+                } else {
+                  this.rappelDuCoursImage = '../assets/img/' + objectif.rappelDuCoursImage
                 }
-                if (trouve) break
+                this.slugVideo = objectif.slugVideo
+                this.VideoSrc = "https://www.youtube.com/embed/" + this.slugVideo
+                this.auteurVideo = objectif.auteurVideo
+                this.lienAuteurVideo = objectif.lienAuteurVideo
+                this.exercices = [] // Au cas où l'attribut ne serait pas réinitialisé lors d'un changement de référence
+                // Le nombre d'exercices varie selon la référence, on a donc quelque chose de dynamique
+                for (let i = 0; i < objectif.exercices.length; i++) {
+                  if (objectif.exercices[i].slug != '') {
+                    this.exercices.push({
+                      couleur: objectif.exercices[i].couleur,
+                      slug: objectif.exercices[i].slug,
+                      lien: 'https://coopmaths.fr/exercice.html?ex=' + objectif.exercices[i].slug + 'i=0&v=e'
+                    })
+                  }
+                }
               }
-              if (trouve) break
-            }
-            if (trouve) break
-          }
-          if (trouve) break
-        }
-      }
-    )
+              return objectif.reference == this.reference;
+            })
+          })
+        })
+      })
+    })
   }
-
 }
--- src/app/objectifs/objectifs.component.ts
+++ src/app/objectifs/objectifs.component.ts
@@ -1,33 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+interface Lien {
+  reference: string;
+  titre: string
+}
 @Component({
   selector: 'app-objectifs',
   templateUrl: './objectifs.component.html',
   styleUrls: ['./objectifs.component.css']
 })
 export class ObjectifsComponent implements OnInit {
-  references: string[]
-  titres: string[]
+  liens: Lien[]
 
   constructor(public http: HttpClient) {
-    this.references = []
-    this.titres = []
+    this.liens = []
   }
   
   ngOnInit(): void {
       this.http.get('assets/data/objectifs.json').subscribe(
-        (data) => {
-          let json = JSON.parse(JSON.stringify(data))
-          this.references = []
-          for (let niveau in json) {
-            for (let theme in json[niveau]) {
-              for (let sousTheme in json[niveau][theme]) {
-                Object.keys(json[niveau][theme][sousTheme]).forEach((element: string) => {
-                  this.references.push(element);
-                });
-                for (let reference in json[niveau][theme][sousTheme]) {
-                  this.titres.push(json[niveau][theme][sousTheme][reference].titre)
+        (data : any) => {
+          this.liens = []
+          for (const niveau of data) {
+            for (const theme of niveau.themes) {
+              for (const sousTheme of theme.sous_themes) {
+                for (const reference of sousTheme.objectifs) {
+                  this.liens.push(reference)
                 }
               }
             }
