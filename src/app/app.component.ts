0e473f5696f4c882d7156360fee950b95cc7aa12
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-20 11:51:00 +0400

    feat: nouveau bandeau de navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationStart, Event as NavigationEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../assets/css/mystyles.css']
 })
-export class AppComponent {
-  title = 'Ah ok';
+export class AppComponent implements OnDestroy {
+  ongletActif: string
+  event$: any
+  constructor(private router: Router) {
+    this.ongletActif = 'accueil'
+    this.event$ = this.router.events.subscribe((event: NavigationEvent) => {
+      if (event instanceof NavigationStart) {
+        this.ongletActif = event.url.split('/')[1]
+      }
+    });
+  }
+  ngOnDestroy() {
+    this.event$.unsubscribe();
+  }
 }
