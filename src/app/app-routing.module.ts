22be3d269980f38ff89d18fe66e47ae0b9477e79
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-20 12:14:40 +0400

    feat: permet de filtrer les objectifs par niveau

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,10 @@ import { ObjectifsComponent } from './objectifs/objectifs.component';
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'objectif/:ref', component: ObjectifComponent },
-  { path: 'objectifs', component: ObjectifsComponent },
+  { path: 'objectifs/:niveau/:theme/:sous_theme', component: ObjectifsComponent },
+  { path: 'objectifs/:niveau/:theme', component: ObjectifsComponent },
+  { path: 'objectifs/:niveau', component: ObjectifsComponent },
+  { path: 'objectifs', redirectTo: 'objectifs/tout' },
   { path: ':ref', component: ObjectifComponent }
 ];
 
--- src/app/objectifs/objectifs.component.ts
+++ src/app/objectifs/objectifs.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap, NavigationStart, Event as NavigationEvent } from '@angular/router';
 
 /**
  * Type d'objet de toutes les lignes qui seront affichées
@@ -21,29 +22,48 @@ interface Ligne {
 })
 export class ObjectifsComponent implements OnInit {
   lignes: Ligne[]
+  filtre: Ligne
+  event$: any
+  ongletActif: string
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient, private route: ActivatedRoute, private router: Router) {
     this.lignes = []
+    this.filtre = {}
+    this.ongletActif = 'tout'
+    this.event$ = this.router.events.subscribe((event: NavigationEvent) => {
+      if (event instanceof NavigationStart) {
+        this.ongletActif = event.url.split('/')[2]
+      }
+    });
   }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.filtre.niveau = params.niveau
+      this.filtre.theme = params.theme
+      this.filtre.sous_theme = params.sous_theme
+    })
     this.http.get('assets/data/objectifs.json').subscribe(
       (data: any) => {
         this.lignes = [] // va contenir toutes les lignes à afficher.
         for (const niveau of data) {
           this.lignes.push({ niveau: niveau.niveau })
           for (const theme of niveau.themes) {
-            this.lignes.push({ theme: theme.nom })
+            this.lignes.push({ niveau: niveau.niveau, theme: theme.nom })
             for (const sous_theme of theme.sous_themes) {
-              this.lignes.push({ sous_theme: sous_theme.nom })
-              for (const reference of sous_theme.objectifs) {
-                this.lignes.push(reference)
+              this.lignes.push({ niveau: niveau.niveau, theme: theme.nom, sous_theme: sous_theme.nom })
+              for (const objectif of sous_theme.objectifs) {
+                this.lignes.push({niveau: niveau.niveau, theme: theme.nom, sous_theme: sous_theme.nom, reference: objectif.reference, titre: objectif.titre})
               }
             }
           }
         }
       }
     )
+  }
+  
+  ngOnDestroy() {
+    this.event$.unsubscribe();
   }
 
 }
