062f830bead67f07d55d42526db18fab420bfbca
Guillaume Valmont <guillaume.valmont@pm.me>
2021-10-23 16:37:41 +0400

    chore: Règle le soucis d'absence de css lorsque le innerHTML est modifié

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Router, NavigationStart, Event as NavigationEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../assets/css/mystyles.css']
+  styleUrls: ['../assets/css/mystyles.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnDestroy {
   ongletActif: string
