00fde50e265287ec51195713d6c4a6b641d45666
Guillaume Valmont <guillaume.valmont@pm.me>
2021-11-06 12:29:16 +0400

    feat: Les boutons de l'accueil apparaissent maintenant tous en colonne lors d'un affichage en portrait

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../../assets/css/mystyles.css']
 })
 export class AccueilComponent implements OnInit {
-
-  constructor() { }
+  isPortrait : boolean
+  constructor() {
+    this.isPortrait = true
+    this.isPortraitUpdate()
+  }
 
   ngOnInit(): void {
   }
 
+  /**
+   * On détecte les changements de taille de fenêtre,
+   * et on ajuste la largeur des cartes en conséquence.
+   * @param event
+   */
+   @HostListener('window:resize', ['$event'])
+   onResize(event: any) {
+     this.isPortraitUpdate()
+   }
+
+   /**
+    * Vérifie si l'écran est en portrait ou en paysage
+    * et met à jour this.isPortrait
+    */
+   isPortraitUpdate() {
+    window.innerHeight > window.innerWidth ? this.isPortrait = true : this.isPortrait = false
+   }
 }
