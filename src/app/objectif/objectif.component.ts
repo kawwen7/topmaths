6d639a8ca63e723a3c8e2f9c5bf5a303708fe8bb
Guillaume Valmont <guillaume.valmont@pm.me>
2021-11-07 06:12:29 +0400

    chore: L'url copiée est différente si l'exercice est interactif ou pas

--- src/app/objectif/objectif.component.ts
+++ src/app/objectif/objectif.component.ts
@@ -32,6 +32,7 @@ export class ObjectifComponent implements OnInit {
   exercices: Exercice[]
   lienFiche: string
   lienAnki: string
+  interactif = false
 
   constructor(public http: HttpClient, private route: ActivatedRoute) {
     this.reference = ''
@@ -59,7 +60,12 @@ export class ObjectifComponent implements OnInit {
         for (const exercice of this.exercices) {
           if (typeof (exercice.lienACopier) != 'undefined') {
             if (url.split('&serie=')[0].split(',i=')[0] == exercice.lienACopier.split('&serie=')[0].split(',i=')[0]) { // Lorsqu'un exercice n'est pas interactifReady, le ,i=0 est retiré de l'url
-              exercice.lienACopier = url
+              exercice.graine = event.data.graine
+              if (this.interactif) { // Si on est en interactif, on retire l'userId et on ajoute la graine
+                exercice.lienACopier = url.split('&userId=')[0] + '&serie=' + exercice.graine
+              } else {
+                exercice.lienACopier = url
+              }
             }
           }
         }
@@ -129,6 +135,12 @@ export class ObjectifComponent implements OnInit {
       }
     }
     this.exercices = [] // Au cas où l'attribut ne serait pas réinitialisé lors d'un changement de référence
+    let userId = ''
+    let i = 'i=0'
+    if (this.interactif) {
+      userId = '&userId=VAL00000'
+      i = 'i=1'
+    }
     // Le nombre d'exercices varie selon la référence, on a donc quelque chose de dynamique
     for (const exercice of objectif.exercices) {
       if (exercice.slug != '') {
@@ -137,7 +149,7 @@ export class ObjectifComponent implements OnInit {
           couleur: '',
           slug: exercice.slug,
           graine: exercice.graine,
-          lien: `https://coopmaths.fr/mathalea.html?ex=${exercice.slug},i=0&serie=${exercice.graine}&v=embed&p=1.5`
+          lien: `https://coopmaths.fr/mathalea.html?ex=${exercice.slug},${i}&serie=${exercice.graine}&v=embed&p=1.5${userId}`
         })
         this.exercices[this.exercices.length - 1].lienACopier = this.exercices[this.exercices.length - 1].lien
         if (exercice.slug.slice(0, 4) == 'http') {
